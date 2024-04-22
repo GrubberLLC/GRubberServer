2db7b2ca4d6e4916829bcc1bf02e3bbdc55a7e83
omar jandali <omarjandali93@gmail.com>
2024-04-21 20:38:39 -0700

    Omar Jandali.  Updated to new database

--- bin/utils/AwsDbConnect.js
+++ bin/utils/AwsDbConnect.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const postgres = require('postgres');
 
 // Database configuration
 const dbConfig = {
@@ -9,16 +9,6 @@ const dbConfig = {
   port: process.env.RDS_PORT
 };
 
-// Create a connection to the database
-const connection = mysql.createConnection(dbConfig);
-
-// Connect to the database
-connection.connect(err => {
-  if (err) {
-    console.error('Error connecting to the database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to the database.');
-});
+const connection = postgres(dbConfig)
 
 module.exports = connection;
