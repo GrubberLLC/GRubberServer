0c0ba97288199f06651b163e3b19139470e0c24c
omar jandali <omarjandali93@gmail.com>
2024-04-27 08:55:17 -0700

    omar jandali. updated profiles sql

--- Controllers/ProfileController.js
+++ Controllers/ProfileController.js
@@ -6,11 +6,10 @@ const ProfileController = {
       last_name, name, bio, nickname, profile_picture, public, notifications } = req.body;
     const query = `
       INSERT INTO Profiles
-      (userId, username, email, phone, location, first_name, last_name, 
+      (user_id, username, email, phone, location, first_name, last_name, 
         full_name, profile_picture, bio, public, nickname, notifications, following, followers)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, 0, 0)
       RETURNING *;`;
-
     try {
       const result = await pool.query(query, [userId, username, email, phone, location, 
         first_name, last_name, name, profile_picture, bio, public, nickname, notifications]);
