55848d3cf916df97c78528ebe99449b4a6aa4830
omar jandali <omarjandali93@gmail.com>
2024-05-07 02:21:56 -0700

    Omar Jandali.  Omar ZJandali. updated places endpoints

--- Controllers/PlaceController.js
+++ Controllers/PlaceController.js
@@ -1,37 +1,38 @@
 const connection = require('../bin/utils/AwsDbConnect'); // Adjust the path as necessary
 
 const PlaceController = {
-  createPlace: (req, res) => {
-    const { name, phone, address_street, address_city, address_state, address_zipcode,
-            address_formatted, rating, review_count, picture, price, categories, 
-            yelp_id, yelp_url } = req.body; 
+  createPlace: async (req, res) => {
+    const { name, phone, price, rating, review_count, closed, address_street, address_city, 
+      address_state, address_zip_code, address_formatted, yelp_url, yelp_id, 
+      image, longitude, latitude } = req.body; 
     const query = `
-      INSERT INTO Places 
-        (name, phone, address_street, address_city, address_state, address_zipcode,
-          address_formatted, rating, review_count, picture, price, categories, 
-          yelp_id, yelp_url, created_at)
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())`
-    connection.query(query, [name, phone, address_street, address_city, 
-      address_state, address_zipcode, address_formatted, rating,
-      review_count, picture, price, categories, yelp_id, yelp_url], (err, results) => {
-        if(err){
-          return res.status(500).send(err.message);
-        } 
-        console.log(JSON.stringify(results))
-        return res.status(201).send({id: results.insertId})
-    });
+      INSERT INTO Profiles
+      (name, phone, price, rating, review_count, closed, address_street, address_city, address_state, 
+        address_zip_code, address_formatted, yelp_url, yelp_id, image, longitude, latitude, created_at)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, NOW())
+      RETURNING *;`;
+    try {
+      const result = await pool.query(query, [name, phone, price, rating, review_count, closed, 
+        address_street, address_city, address_state, address_zip_code, address_formatted, yelp_url, yelp_id, 
+        image, longitude, latitude]);
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err.message);
+    }
   },
-  getPlaceById: (req, res) => {
+  getPlaceById: async (req, res) => {
     const {id} = req.params; 
     const query = `
-      SELECT * FROM Places WHERE place_id = ?
+      SELECT * FROM Places WHERE place_id = $1 RETURNING *;
     `
-    connection.query(query, [id], (err, results) => {
-        if(err){
-          return res.status(500).send(err.message);
-        } 
-        return res.status(201).send(results)
-    });
+    try {
+      const result = await pool.query(query, [id]);
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err.message);
+    }
   },
   getPlaceByYelpId: (req, res) => {
     const {id} = req.params; 
