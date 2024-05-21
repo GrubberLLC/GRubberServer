b0ea5a2d60aebabeb4ac713ae31dd4563591d00a
omar jandali <omarjandali93@gmail.com>
2024-05-21 16:01:39 -0700

    Omar Jandali. fixed the query

--- Controllers/PostCommentController.js
+++ Controllers/PostCommentController.js
@@ -37,8 +37,7 @@ const PostCommentController = {
       SELECT Comments.*, Profiles.*
       FROM Comments
       JOIN Profiles ON Profiles.user_id = Comments.user_id
-      WHERE Comment.post_id = $1
-      RETURNING *;
+      WHERE Comment.post_id = $1;
     `
     console.log(query)
     try {
