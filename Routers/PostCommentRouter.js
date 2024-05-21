1ff3b0ebcdd8cca4d02ed7a6c548583d1936cb00
omar jandali <omarjandali93@gmail.com>
2024-05-21 10:38:51 -0700

    Omar Jandali. updated the endpoint router

--- Routers/PostCommentRouter.js
+++ Routers/PostCommentRouter.js
@@ -3,7 +3,7 @@ const { PostCommentController } = require('../Controllers/PostCommentController'
 const PostCommentRouter = express.Router();
 
 PostCommentRouter.route('')
-  .post(PostCommentController.createPost)
+  .post(PostCommentController.createPostCOmment)
 
 PostCommentRouter.route('/:id')
   .get(PostCommentController.getCommentByPostId)
