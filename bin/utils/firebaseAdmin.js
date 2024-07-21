60aedcaac22915aa6103120cc54ad885e6dfacbf
omar jandali <omarjandali93@gmail.com>
2024-07-21 11:57:46 -0700

    OPmar Jandali. Adjusted key s

--- bin/utils/firebaseAdmin.js
+++ bin/utils/firebaseAdmin.js
@@ -1,7 +1,7 @@
 const admin = require('firebase-admin');
-
-const serviceAccountBase64 = process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64;
-const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf8'));
+require('dotenv').config();
+// const serviceAccount = require('./grubber-81361-4727726bcab3.json');
+const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
