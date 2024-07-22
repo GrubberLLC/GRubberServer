46a5602995db43784d83dffb8b6c1b13a3cd66e6
omar jandali <omarjandali93@gmail.com>
2024-07-21 21:11:21 -0700

    Omar Jandali. adjusted server to decode base 64 document

--- bin/utils/firebaseAdmin.js
+++ bin/utils/firebaseAdmin.js
@@ -1,7 +1,9 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
+
 // const serviceAccount = require('./grubber-81361-4727726bcab3.json');
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS
+const serviceAccountBase64 = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountBase64, 'base64').toString('utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
