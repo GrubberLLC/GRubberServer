da37c3248abd464b3860c85cae6351452b11255f
omar jandali <omarjandali93@gmail.com>
2024-08-06 11:40:20 -0700

    Omar Jandali. added sending email to server side rather than client side

--- Controllers/ReportController.js
+++ Controllers/ReportController.js
@@ -1,8 +1,19 @@
 const pool = require('../bin/utils/AwsDbConnect'); // Adjust the path as necessary
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.example.com', // Replace with your SMTP server
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.GMAIL_USERNAME, // Replace with your email
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
 
 const ReportController = {
   createReport: async (req, res) => {
-    const { reporter_id, reported_id, subject, message, category } = req.body; 
+    const { reporter_id, reporter_email, reported_id, subject, message, category } = req.body; 
     const query = `
       INSERT INTO Places
       (reporter_id, reported_id, subject, message, category, created_at)
@@ -10,6 +21,25 @@ const ReportController = {
       RETURNING *;`;
     try {
       const result = await pool.query(query, [reporter_id, reported_id, subject, message, category]);
+
+      const mailOptions = {
+        from: 'contact@grubber.io', // sender address
+        to: 'report@grubber.io', // list of receivers
+        cc: reporter_email, // optional CC
+        subject: subject,
+        text: message, // plain text body
+        html: `<p>${message.replace(/\n/g, '<br>')}</p>` // HTML body
+      };
+
+      // Send email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error('Error sending email:', error);
+          return res.status(500).send('Error sending email');
+        }
+        console.log('Email sent:', info.response);
+      });
+
       res.status(201).json(result.rows);
     } catch (err) {
       console.error(err);
